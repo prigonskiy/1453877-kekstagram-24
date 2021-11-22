def1da435fe95d745228ea7bcd809b42459ebaa0
Filipp Prigonskiy <jerk1991@gmail.com>
2021-11-22 18:34:51 +0300

    Починил открытие экрана редактирования изображения

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 import { getData } from './api.js';
 import { listenFilters } from './filter.js';
 import { createThumbnails } from './thumbnails.js';
-import { listenThumbnails } from './gallery.js';
+import { listenThumbnails, listenUploadForm } from './gallery.js';
 
 const startApp = async () => {
   const picturesArray = await getData();
   createThumbnails(picturesArray);
   listenThumbnails(picturesArray);
   listenFilters(picturesArray);
+  listenUploadForm();
 };
 
 export { startApp, createThumbnails };
